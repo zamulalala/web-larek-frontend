c95b23b9c5e64c558104e6fb3d1b1aead67815d1
zamulalala <ritazamula2001@gmail.com>
2024-03-23 15:22:13 +0300

    iteration 1.1 documentation

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
+// Интерфейс, описывающий структуру продукта
 export interface IProduct {
   id: string;
   description?: string;
@@ -7,6 +8,7 @@ export interface IProduct {
   price: string;
 }
 
+// Интерфейс, описывающий глобальное состояние приложения
 export interface IAppState {
   catalog: IProduct[];
   basket: string[];
@@ -14,25 +16,30 @@ export interface IAppState {
   order: {} | null;
 }
 
+// Интерфейс для UI успешной покупки
 export interface ISuccessfulPurchaseUI {
   total: number;
 }
 
+// Интерфейс для UI модального окна
 export interface IModalUI {
   content: HTMLElement;
 }
 
+// Интерфейс для UI корзины покупок
 export interface IBasketUI {
   items: HTMLElement[];
   total: number;
   selected: string[];
 }
 
+// Интерфейс для UI главной страницы
 export interface IPageUI {
   counter: number;
   catalog: HTMLElement[];
 }
 
+// Интерфейс для UI отображения продукта
 export interface IProductUI {
   id: string;
   description?: string;

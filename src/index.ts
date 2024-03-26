132988509f25278c9aa9989e9aecdba29aa193b7
zamulalala <ritazamula2001@gmail.com>
2024-03-27 00:54:11 +0300

    iteration 2.0 - change READ.ME

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-import './scss/styles.scss';
+import 'src/scss/styles.scss';
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -13,7 +13,24 @@ export interface IAppState {
   catalog: IProduct[];
   basket: string[];
   preview: string | null;
-  order: {} | null;
+  order: IOrder | null;
+}
+
+// Интерфейс для UI формы оплаты
+export interface IPaymentFormUI {
+  payment: string;
+  address: string;
+}
+
+// Интерфейс для UI формы контактов
+export interface IContactsFormUI {
+  email: string;
+  tel: string;
+}
+
+// Интерфейс для данных о заказе
+export interface IOrder extends IPaymentFormUI, IContactsFormUI {
+  items: string[]
 }
 
 // Интерфейс для UI успешной покупки
